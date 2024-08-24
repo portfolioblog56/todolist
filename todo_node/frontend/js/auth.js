f76d76b90c8103c627c9f787e994ab96c155268d
portfolioblog56 <portfolioblog56@gmail.com>
2024-08-24 22:16:15 +0530

    test_app

--- todo_node/frontend/js/auth.js
+++ todo_node/frontend/js/auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://todolist-uz9d.onrender.com/api';
 
 const authForm = document.getElementById('auth-form');
 const usernameInput = document.getElementById('username');
